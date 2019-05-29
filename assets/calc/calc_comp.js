8a2f7269faa77aa993abb0a0bd9d222adabd8e6e
patentrf <49150621+patentrf@users.noreply.github.com>
2019-05-29 18:11:41 +0500

    Add files via upload

---
+++ assets/calc/calc_comp.js
@@ -0,0 +1,27 @@
+		var a, b, c, job, dutySum, finalSum, tradeMarkSelect;
+			var a = 3000;
+			var b = 4500;
+			var job = 10000;
+		window.onload = function() {
+			dutySum = document.getElementById("duty_sum");
+			finalSum = document.getElementById("Sum");
+			tradeMarkSelect = document.getElementById("tm");
+			typeSelect = document.getElementById("type");
+
+
+						onCalculate();
+			}
+			onCalculate();
+
+			
+			function onCalculate() {
+			if (document.getElementById('type2').value == "2") {duty_sum.innerHTML = a} else {duty_sum.innerHTML = b};
+			//пошлина
+			
+			job_sum.innerHTML = ~~job;
+			//стоимость работ
+
+			Sum.innerHTML = (~~job) + (~~(duty_sum.innerHTML));
+			//итого
+
+		}
---
+++ assets/calc/calc_um.js
@@ -0,0 +1,53 @@
+var c, price, dutySum, finalSum, tradeMarkSelect;
+//пункт 1.2
+	var inval1 = 98;
+	var alone1 = 245;
+	var sub1 = 343;
+	var ip1 = 980;
+
+//пункт 1.10
+	var inval2 = 175;
+	var alone2 = 437.5;
+	var sub2 = 612.5;
+	var ip2 = 1750;
+
+//пункт 1.18
+	var default3 = 2100;
+	var inval3 = 210;
+	var alone3 = 525;
+	var sub3 = 735;
+	var ip3 = 2100;
+
+//пункт 1.19.1
+	var poshlina19 = 1050;
+
+
+	window.onload = function() {
+		dutySum = document.getElementById("duty_sum");
+		finalSum = document.getElementById("Sum");
+		tradeMarkSelect = document.getElementById("tm").value;
+		typeSelect = document.getElementById("type");
+		onCalculate();
+		}
+	onCalculate();
+
+	function onCalculate() {
+
+			if (document.getElementById('tm').value == "1") {duty_sum.innerHTML = ip2+ip1};
+			if (document.getElementById('tm').value == "2") {duty_sum.innerHTML = inval2+inval1};
+			if (document.getElementById('tm').value == "3") {duty_sum.innerHTML = alone2+alone1};
+			if (document.getElementById('tm').value == "4") {duty_sum.innerHTML = sub2+sub1};
+			if (document.getElementById('tm').value == "5") {duty_sum.innerHTML = ip2+ip1};
+			//пошлина по п. 1.2 и п. 1.10
+
+			if (document.getElementById('tm').value == "1") {job_sum.innerHTML = poshlina19+ip3};
+			if (document.getElementById('tm').value == "2") {job_sum.innerHTML = poshlina19+inval3};
+			if (document.getElementById('tm').value == "3") {job_sum.innerHTML = poshlina19+alone3};
+			if (document.getElementById('tm').value == "4") {job_sum.innerHTML = poshlina19+sub3};
+			if (document.getElementById('tm').value == "5") {job_sum.innerHTML = poshlina19+ip3};
+			//пошлина по п. 1.18 и п. 1.19.1
+
+			Sum.innerHTML = (~~job_sum.innerHTML) + (~~(duty_sum.innerHTML));
+			//итого
+};
+		
