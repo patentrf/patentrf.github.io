aab59e45e4f310ebf4a006c09c008de92fa704b9
patentrf <49150621+patentrf@users.noreply.github.com>
2019-05-29 19:54:55 +0500

    Update calc_um.js

--- assets/calc/calc_um.js
+++ assets/calc/calc_um.js
@@ -30,24 +30,26 @@ var c, price, dutySum, finalSum, tradeMarkSelect;
 		onCalculate();
 		}
 	onCalculate();
-
 	function onCalculate() {
 
-			if (document.getElementById('tm').value == "1") {duty_sum.innerHTML = ip2+ip1};
-			if (document.getElementById('tm').value == "2") {duty_sum.innerHTML = inval2+inval1};
-			if (document.getElementById('tm').value == "3") {duty_sum.innerHTML = alone2+alone1};
-			if (document.getElementById('tm').value == "4") {duty_sum.innerHTML = sub2+sub1};
-			if (document.getElementById('tm').value == "5") {duty_sum.innerHTML = ip2+ip1};
+			if (document.getElementById('tm').value == "1") {a = ip2+ip1};
+			if (document.getElementById('tm').value == "2") {a = inval2+inval1};
+			if (document.getElementById('tm').value == "3") {a = alone2+alone1};
+			if (document.getElementById('tm').value == "4") {a = sub2+sub1};
+			if (document.getElementById('tm').value == "5") {a = ip2+ip1};
 			//пошлина по п. 1.2 и п. 1.10
 
-			if (document.getElementById('tm').value == "1") {job_sum.innerHTML = poshlina19+ip3};
-			if (document.getElementById('tm').value == "2") {job_sum.innerHTML = poshlina19+inval3};
-			if (document.getElementById('tm').value == "3") {job_sum.innerHTML = poshlina19+alone3};
-			if (document.getElementById('tm').value == "4") {job_sum.innerHTML = poshlina19+sub3};
-			if (document.getElementById('tm').value == "5") {job_sum.innerHTML = poshlina19+ip3};
+			if (document.getElementById('tm').value == "1") {b = poshlina19+ip3};
+			if (document.getElementById('tm').value == "2") {b = poshlina19+inval3};
+			if (document.getElementById('tm').value == "3") {b = poshlina19+alone3};
+			if (document.getElementById('tm').value == "4") {b = poshlina19+sub3};
+			if (document.getElementById('tm').value == "5") {b = poshlina19+ip3};
 			//пошлина по п. 1.18 и п. 1.19.1
 
-			Sum.innerHTML = (~~job_sum.innerHTML) + (~~duty_sum.innerHTML);
+			
+			duty_sum.innerHTML = a;
+			job_sum.innerHTML = b;
+
+			Sum.innerHTML = a + b;
 			//итого
 };
-		
